369b17d34ee88d1c53f28f515bba454b5712d0b2
Fletcher Haynes <fletcher@unity3d.com>
2017-12-20 20:10:10 -0800

    Started on an Elm-based UI

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -5,13 +5,14 @@ extern crate serde_json;
 extern crate futures;
 extern crate hyper;
 extern crate regex;
+
 pub mod handlers;
 pub mod router;
 
 
 use futures::future::Future;
 
-use hyper::server::{Http, Request, Response, Service};
+use hyper::server::{Request, Response, Service};
 use hyper::StatusCode;
 
 pub struct Saga {
@@ -19,12 +20,9 @@ pub struct Saga {
 }
 
 impl Service for Saga {
-    // boilerplate hooking up hyper's server types
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
-    // The future representing the eventual Response your call will
-    // resolve to. This can change to whatever Future you need.
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
@@ -40,7 +38,6 @@ impl Service for Saga {
                 ))
             }
         }
-
     }
 }
 
