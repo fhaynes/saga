3c97c79d61374207e9c32d0148a0c3e9e9c8905c
Fletcher Haynes <fletcher@unity3d.com>
2017-12-22 19:52:31 -0800

    Removed some tarpc remnants

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,10 +1,7 @@
-#![feature(plugin, use_extern_macros)]
-#![plugin(tarpc_plugins)]
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
-extern crate tarpc;
 
 extern crate futures;
 extern crate hyper;
